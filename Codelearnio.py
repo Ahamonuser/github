7a0ae0103759f2211453c22f61c56ea467f98842
Ahamonuser <116050616+Ahamonuser@users.noreply.github.com>
2022-10-18 10:08:36 +0700

    Add files via upload

---
+++ Codelearnio.py
@@ -0,0 +1,11 @@
+temparature = int(input())
+if temparature >= 100:
+    print ("Stay at home and enjoy a good movie")
+elif temparature >= 92:
+    print ("Stay at home")
+elif temparature == 75:
+    print ("Go outside and enjoy the weather")
+elif temparature < 0:
+    print ("It's cool outside")
+else:
+    print ("Let's go to school")
